03cf42d6b7106fa899b71564c40451a26fcf15c1
tsutrof <jaysonfortus0815@gmail.com>
2023-08-02 15:32:43 +0800

    first commit

---
+++ dist/js/script.js
@@ -0,0 +1,6 @@
+const toggleMenu = document.querySelector('.toggle__menu');
+const headerNav = document.querySelector('.header__nav');
+
+toggleMenu.addEventListener('click', () =>{
+    headerNav.classList.toggle('h-[20rem]');
+});
